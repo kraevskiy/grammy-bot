cbe7833c56fa7678b8dcdcf9d3bc2a7929f5af63
rambutan <pride.kraievskyi@gmail.com>
2022-11-30 16:02:52 +0200

    delete link

--- src/commands/message/index.ts
+++ src/commands/message/index.ts
@@ -4,6 +4,7 @@ import { sendMarkdown } from "../../core/sendMarkdown";
 import { sendMedia } from "../../core/sendMedia";
 import { markdownLink } from "../../helpers/markdown.link";
 import { Context } from "grammy";
+import { IParseSuccess } from "../../types";
 
 const message = async (ctx: Context): Promise<void> => {
   const message = ctx.message;
@@ -14,21 +15,27 @@ const message = async (ctx: Context): Promise<void> => {
     await ctx.reply(en.waitPls);
     const res = await parseInfo(message.text, ctx);
     if (res) {
-      if ("error" in res) {
-        await ctx.reply(en[res.error]);
-        await ctx.reply(en.pasteVin);
-      } else {
-        await sendMarkdown(message.chat.id, res);
-        await ctx.reply(en.waitPhotos);
-        await sendMedia(message.chat.id, res);
-        if (res.data.allPhotos) {
-          await ctx.reply(markdownLink(res.data.allPhotos), {
-            parse_mode: "HTML",
-          });
-        }
-        await ctx.reply(en.done);
-        await ctx.reply(en.pasteVin);
-      }
+      await sendMarkdown(message.chat.id, res as IParseSuccess);
+      await ctx.reply(en.waitPhotos);
+      await sendMedia(message.chat.id, res as IParseSuccess);
+      await ctx.reply(en.done);
+      await ctx.reply(en.pasteVin);
+      console.log("error in res", "error" in res);
+      // if ("error" in res) {
+      //   await ctx.reply(en[res.error]);
+      //   await ctx.reply(en.pasteVin);
+      // } else {
+      //   await sendMarkdown(message.chat.id, res);
+      //   await ctx.reply(en.waitPhotos);
+      //   await sendMedia(message.chat.id, res);
+      //   if (res.data.allPhotos) {
+      //     await ctx.reply(markdownLink(res.data.allPhotos), {
+      //       parse_mode: "HTML",
+      //     });
+      //   }
+      //   await ctx.reply(en.done);
+      //   await ctx.reply(en.pasteVin);
+      // }
     } else {
       await ctx.reply(en.error);
     }
