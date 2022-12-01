afb51195c06464226714c0b30cf4b7ebfd15c2e1
rambutan <pride.kraievskyi@gmail.com>
2022-12-01 09:17:41 +0200

    delete node-fetch-commonjs

--- src/helpers/parse.info.ts
+++ src/helpers/parse.info.ts
@@ -1,24 +1,24 @@
 import { IParseSuccess, IParseError } from "../types";
 import { Context } from "grammy";
 import { en } from "../translation";
-import fetch from "node-fetch-commonjs";
+// import fetch from "node-fetch-commonjs";
 
 export const parseInfo = async (
   vin: string,
   ctx: Context
 ): Promise<IParseSuccess | IParseError | null> => {
-  const res = await fetch(process.env.PARSE_HOST as string, {
-    method: "post",
-    headers: {
-      Authorization: process.env.PARSE_AUTH as string,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ vin }),
-  }).then((res) => res.json());
+  // const res = await fetch(process.env.PARSE_HOST as string, {
+  //   method: "post",
+  //   headers: {
+  //     Authorization: process.env.PARSE_AUTH as string,
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({ vin }),
+  // }).then((res) => res.json());
 
-  if (res) {
-    return res as IParseSuccess | IParseError;
-  }
+  // if (res) {
+  //   return res as IParseSuccess | IParseError;
+  // }
   await ctx.reply(en.error);
   return null;
 };
