3dcf4dc14934934004a5acc8dcb98eaeca2506fb
rambutan <pride.kraievskyi@gmail.com>
2022-12-01 09:01:26 +0200

    add express

--- api/index.ts
+++ api/index.ts
@@ -1,25 +1,13 @@
 require("../src/index");
 
+import express from "express";
 import { webhookCallback } from "grammy";
+
 import bot from "../src/core/bot";
 
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
+const app = express();
 
-      return resolve(result);
-    });
-  });
-}
+app.use(express.json());
+app.use(`/api/index`, webhookCallback(bot));
 
-async function handler(req, res) {
-  // Run the middleware
-  await runMiddleware(req, res, webhookCallback(bot));
-}
-
-export default handler;
+export default app;
