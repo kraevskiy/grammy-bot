d37c8852b8d96f3556dbf8bb181298fe14382899
rambutan <pride.kraievskyi@gmail.com>
2022-12-01 08:49:08 +0200

    remove all

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,8 @@ require("../src/index");
 import { webhookCallback } from "grammy";
 import bot from "../src/core/bot";
 
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
 function runMiddleware(req, res, fn) {
   return new Promise((resolve, reject) => {
     fn(req, res, (result) => {
@@ -16,6 +18,7 @@ function runMiddleware(req, res, fn) {
 }
 
 async function handler(req, res) {
+  // Run the middleware
   await runMiddleware(req, res, webhookCallback(bot));
 }
 
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,11 +3,13 @@ import { Composer } from "grammy";
 import start from "./start";
 import help from "./help";
 import message from "./message";
+import hello from "./hello";
 
 const composer = new Composer();
 
-composer.command("start", start);
-composer.command("help", help);
-composer.on("message", message);
+// composer.command("start", start);
+composer.command("hello", hello);
+// composer.command("help", help);
+// composer.on("message", message);
 
 export default composer;
